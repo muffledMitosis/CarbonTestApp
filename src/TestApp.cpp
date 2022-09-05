6ace20cf24f75e0b2a476aff155b05cefa6b4bef
Meth Munindradasa <methsndr@gmail.com>
2022-09-05 07:32:08 +0530

    Make everything easier to debug. Carbon module changes

--- src/TestApp.cpp
+++ src/TestApp.cpp
@@ -10,12 +10,9 @@
 #include <iomanip>
 #include <thread>
 
-#include <imgui.h>
-#include <backends/imgui_impl_glfw.h>
-#include <backends/imgui_impl_opengl3.h>
 
 Carbon::Module::Serial* serial;
-Carbon::Window* window;
+Carbon::ApplicationWindow* window;
 float f;
 int pwm;
 
@@ -37,46 +34,21 @@ std::ostringstream currentTimeStream;
 
 void TestApp::Init()
 {
+  glfwInit();
   serial = new Carbon::Module::Serial({9600, "/dev/ttyUSB0"});
-  window = new Carbon::Window();
+  window = new Carbon::ApplicationWindow();
   this->ExitCondition = !glfwWindowShouldClose(window->window);
-  // glClearColor(1.0, 0, 0, 0);
-  ImGui::CreateContext();
-  ImGui_ImplGlfw_InitForOpenGL(window->window, true);
-  ImGui_ImplOpenGL3_Init();
-
+   glClearColor(1.0, 0, 0, 0);
 }
 
 void TestApp::Update()
 {
   this->ExitCondition = !glfwWindowShouldClose(window->window);
-  // serial->writeTestArduino();
+   serial->writeTestArduino();
   glClear(GL_COLOR_BUFFER_BIT);
-  ImGui_ImplOpenGL3_NewFrame();
-  ImGui_ImplGlfw_NewFrame();
-
-  ImGui::NewFrame();
-
-  ImGui::SetNextWindowBgAlpha(0.7f);
-  // create a window and append into it
-  ImGui::Begin("Controls", NULL, NULL);
-
-  ImGui::Dummy(ImVec2(0.0f, 1.0f));
-  ImGui::TextColored(ImVec4(1.0f, 0.0f, 1.0f, 1.0f), "Time");
-  ImGui::Text("%s", currentTime(std::chrono::system_clock::now()).c_str());
-  if(ImGui::SliderAngle("Le Angle", &f))
-      std::cout<<f<<std::endl;
-  if(ImGui::SliderInt("PWM val", &pwm, 0, 100))
-  {
-    serial->writeInt(pwm);
-  }
-  ImGui::End();
-
-  ImGui::Render();
-  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+  
+  
 
   glfwSwapBuffers(window->window);
   glfwPollEvents();
-
-  serial->readLine();
 }
